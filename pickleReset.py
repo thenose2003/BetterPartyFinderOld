7b23f05bb2d8f02fea900138f1e16928c8cf8791
thenose2003 <36615001+thenose2003@users.noreply.github.com>
2020-11-17 13:51:41 -0700

    Update pickleReset.py
    There was an issue when calling index and the unused floors were all the exact same this sets up some difference.

--- pickleReset.py
+++ pickleReset.py
@@ -3,10 +3,19 @@ import pickle
 dataFolder = 'data'
 
 data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 100]]
-
 pickle.dump(data, open(dataFolder + "/floor1.dat", "wb"))
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 101]]
 pickle.dump(data, open(dataFolder + "/floor2.dat", "wb"))
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 102]]
 pickle.dump(data, open(dataFolder + "/floor3.dat", "wb"))
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 103]]
 pickle.dump(data, open(dataFolder + "/floor4.dat", "wb"))
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 104]]
 pickle.dump(data, open(dataFolder + "/floor5.dat", "wb"))
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 105]]
 pickle.dump(data, open(dataFolder + "/floor6.dat", "wb"))
