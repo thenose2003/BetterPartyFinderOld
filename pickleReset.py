dbcc6bbd7e23ad7eb02b84d07dba2bf68c31b753
thenose2003 <36615001+thenose2003@users.noreply.github.com>
2020-11-25 10:46:57 -0700

    Update pickleReset.py
    changed to 1000

--- pickleReset.py
+++ pickleReset.py
@@ -2,23 +2,23 @@ import pickle
 
 dataFolder = 'data'
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 100]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1000]]
 pickle.dump(data, open(dataFolder + "/floor1.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 101]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1001]]
 pickle.dump(data, open(dataFolder + "/floor2.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 102]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1002]]
 pickle.dump(data, open(dataFolder + "/floor3.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 103]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1003]]
 pickle.dump(data, open(dataFolder + "/floor4.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 104]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1004]]
 pickle.dump(data, open(dataFolder + "/floor5.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 105]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1005]]
 pickle.dump(data, open(dataFolder + "/floor6.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 106]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1006]]
 pickle.dump(data, open(dataFolder + "/floor7.dat", "wb"))
