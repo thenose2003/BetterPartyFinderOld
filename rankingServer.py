e8073ff622a82a6bd9a800c02525a65153d9f288
thenose2003 <36615001+thenose2003@users.noreply.github.com>
2020-12-21 12:37:02 -0700

    Update rankingServer.py

--- rankingServer.py
+++ rankingServer.py
@@ -2,19 +2,23 @@ import pickle
 import rankHandler
 import socket
 import json
+from gameActivationHandler import *
 from multiprocessing import *
 import requests
 
 version = '0.0.5'
 dataFolder = 'data'
 
-HOST = '66.175.233.189'
+HOST = '127.0.0.1'
 PORT = 443
 
 def getUUID(ign):
-    print(ign)
     return requests.get("https://api.mojang.com/users/profiles/minecraft/"+ign).json()['id']
     return -1
+
+def getIGN(uuid):
+    names = requests.get('https://api.mojang.com/user/profiles/' + str(uuid) + '/names').json()
+    return names[len(names)-1]['name']
 
 class rankingServer:
     def __init__(self):
@@ -54,16 +58,14 @@ class rankingServer:
         self.load()
         profile = rH.findProfile(getattr(self, 'floor'+str(floor)), uuid)
         print(getattr(self, 'floor'+str(floor)))
-        print(profile)
         #def newSkill(self, profile, work, totalWork, **kwargs):
         getattr(self, 'floor'+str(floor))[profile][2] = int(rH.newSkill(getattr(self, 'floor'+str(floor))[profile], work, totalWork))
-        #print(getattr(self, 'floor'+str(floor))[profile])
         self.save()
 
     def run(self):
         rH = rankHandler.handler(version)
         while True:
-            try:
+            #try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     s.bind((HOST, PORT))
                     s.listen()
@@ -72,8 +74,8 @@ class rankingServer:
                     self.handleSocket(conn, addr, s, rH)
                     #p = Process(target=self.handleSocket, args=(conn, addr, s,))
                     #p.start()
-            except:
-                print('[server] Error in rankingServer.run()')
+            #except:
+                #print('[server] Error in rankingServer.run()')
 
     def handleSocket(self, conn, addr, s, rH):
         try:
@@ -85,31 +87,59 @@ class rankingServer:
                 #    break
 
                 rData = data
-                rData = rData.split('\\')[0] #saves data
+                print(rData)
+                rData = rData.rstrip() #saves data
                 rData = rData.split(':')
 
-                print(rData)
+                pData = rData
+
+                print(pData)
+
+                #Finds game and sends back players in active game
+                players = findGame(getUUID(rData[0]), self)
+                if players == None:
+                    print('[SERVER] No Game Found')
+                    return
+                else:
+                    send = ''
+                    for i in players:
+                        send += str(getIGN(i))+':'
+                    send = send[:-1]
+                    send += '\n'
+                    conn.sendall(str(send).encode())
+
+                #Recieve client confirmation
+                data = b''
+                while data == b'':
+                    data = conn.recv(100).rstrip()#length of imput
+                rData = data.decode()
+                rData = rData.rstrip()#saves data
+                rData = rData.split(':')
+
+                print('rData: '+str(rData))
+                #for i in rData:
+                #    if getUUID(i) not in players:
+                #        return
+
                 #updates rank
                 #rankChange(self, rH, uuid, floor, work, totalWork):
                 #list = b'NoseThe:1:600:3000
-                self.rankChange(rH, getUUID(rData[0]), int(rData[1]), int(rData[2]), int(rData[3]))
+                self.rankChange(rH, getUUID(pData[0]), int(pData[1]), int(pData[2]), int(pData[3]))
                 self.save()
 
                 #sets the veriable to return
-                profile = rH.findProfile(getattr(self, 'floor'+rData[1]), getUUID(rData[0]))
+                profile = rH.findProfile(getattr(self, 'floor'+pData[1]), getUUID(pData[0]))
                 send = ''
-                for i in getattr(self, 'floor'+rData[1])[profile]:
+                for i in getattr(self, 'floor'+pData[1])[profile]:
                     send += str(i)+':'
                 send = send[:-1]
                 send += '\n'
 
                     #conn.sendall(toBytes(str(send))) #returns data
-                print(send)
                 conn.sendall(str(send).encode())
         except:
-            print("Error in handleSocket")
+            print("[SERVER] Error in handleSocket")
         #s.close()
-
 
 if __name__ == '__main__':
     testList =[ #uuid, skill
