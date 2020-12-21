6d5e9ff0901972a6dfdd63ef9a214cee21168ecd
thenose2003 <36615001+thenose2003@users.noreply.github.com>
2020-12-21 12:35:58 -0700

    Update pickleReset.py
    New files for the new systems put in place

--- pickleReset.py
+++ pickleReset.py
@@ -2,23 +2,34 @@ import pickle
 
 dataFolder = 'data'
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1000]]
+data = [[1, 150, 5, [['Discord1', 'mage', 'healer']], 0]]
+pickle.dump(data, open(dataFolder + "/activeGames.dat", "wb"))
+
+raise Exception("Sorry, no numbers below zero")
+
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 100]]
 pickle.dump(data, open(dataFolder + "/floor1.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1001]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 101]]
 pickle.dump(data, open(dataFolder + "/floor2.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1002]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 102]]
 pickle.dump(data, open(dataFolder + "/floor3.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1003]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 103]]
 pickle.dump(data, open(dataFolder + "/floor4.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1004]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 104]]
 pickle.dump(data, open(dataFolder + "/floor5.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1005]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 105]]
 pickle.dump(data, open(dataFolder + "/floor6.dat", "wb"))
 
-data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 1006]]
+data = [['9983f7e223de466bb66a81120fbf7f9c', 'NotTheNose#6857', 106]]
 pickle.dump(data, open(dataFolder + "/floor7.dat", "wb"))
+
+data = [['NotTheNose#6857', 'mage', 6, 'healer']]
+pickle.dump(data, open(dataFolder + "/oldQueue.dat", "wb"))
+
+data = [[1, 150, 5, [['Discord1', 'mage', 'healer']], 0]]
+pickle.dump(data, open(dataFolder + "/activeGames.dat", "wb"))
