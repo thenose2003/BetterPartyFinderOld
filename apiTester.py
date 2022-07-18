90ca6e967e58ecdbe09eb3ac22bcea058e14441b
thenose2003 <36615001+thenose2003@users.noreply.github.com>
2022-07-18 13:15:07 -0600

    Update apiTester.py

--- apiTester.py
+++ apiTester.py
@@ -1,6 +1,6 @@
 import requests
 
-key = 'fc6c5639-7f38-481f-af0c-f86453fda58a'
+key = '' // Api key
 uuid = 'NoseThe'
 
 f = requests.get('https://api.hypixel.net/player?key='+key+'&name='+uuid).json()
